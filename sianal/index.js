a127cbfa7bf4d64a7ae56c0100355534146bc1f1
liuhao <liuxiansheng2014@sina.com>
2020-03-04 18:23:20 +0800

    信令服务

---
+++ sianal/index.js
@@ -0,0 +1,54 @@
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8010 });
+const code2ws = new Map(); //控制码和ws的一个映射关系存在内存里
+//客户端进行连接
+wss.on('connection', function connection(ws, request) {
+    ws.sendData = (event, data) => {
+        ws.send(JSON.stringify({event, data}));
+    };
+    ws.sendError = msg => {
+        ws.sendData('error', {msg})
+    };
+
+    let code =  Math.floor(Math.random()*(999999-100000)) + 100000;
+    code2ws.set(code, ws)
+    ws.on('message', function incoming(message) {
+        let parsedMessage = {}
+        try {
+            parsedMessage = JSON.parse(message);
+        } catch (e) {
+            console.log('parse error', e)
+            ws.sendError('message not valid')
+            return
+        }
+        // console.log(parsedMessage)
+        let {event, data} =parsedMessage
+        console.log(event)
+        if (event === 'login') {
+            ws.sendData( "logined", {code})
+        } else if(event === 'control'){
+            let remote = +data.remote
+        
+            console.log(code2ws.has(remote))
+            if (code2ws.has(remote)) {
+                ws.sendData('controlled', {remote}) //如果有这个用户那么提示已经控制住了
+                ws.sendRemote = code2ws.get(remote).sendData //随后建立映射
+                ws.sendRemote('be-controlled', {remote: code}) //告诉傀儡端你已经被控制了
+            }
+        } else if (event === 'forward'){
+            ws.sendRemote(data.event, data.data)
+        } else {
+            ws.sendError('message not handle', message)
+        }
+    });
+
+    ws.on('close', () => {
+        code2ws.delete(code)
+        delete ws.sendRemote
+        clearTimeout(ws._closeTimeout);
+    })
+
+    ws._closeTimeout = setTimeout(() => {
+        ws.terminate();
+    }, 600000);
+});
